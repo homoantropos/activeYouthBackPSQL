fc402a38a9ac9938e3cb161416a46b9bec21d3d5
Sergii Antropov <homoantropos@gmail.com>
2022-01-19 12:40:55 +0300

    compression added

---
+++ config/keys.prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    jwt: process.env.jwt,
+    dbName: process.env.dbName,
+    dbUser: process.env.dbUser,
+    dbPwd: process.env.dbPwd,
+    dbHost: process.env.dbHost,
+    dbPort: process.env.dbPort
+}
