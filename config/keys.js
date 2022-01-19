749ee347b298002ef78af3d8c6cb79da9fa46816
Sergii Antropov <homoantropos@gmail.com>
2022-01-19 17:20:55 +0300

    compression added

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     jwt: 'dev-token',
-    dbName: 'activeyouth',
-    dbUser: 'postgres',
-    dbPwd: '20200876',
-    dbHost: 'localhost',
+    dbName: 'd2u7cs903dbvp5',
+    dbUser: 'prdhtjobeijgsv',
+    dbPwd: '672fafc15aac075d5fc9b0d322910c36e852a47af04b850953f7eb0f90c39158',
+    dbHost: 'ec2-52-31-201-170.eu-west-1.compute.amazonaws.com',
     dbPort: '5432'
 }
