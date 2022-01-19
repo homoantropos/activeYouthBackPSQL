36f1ca225441434687f1d9287d8b8e33f7da73fb
Sergii Antropov <homoantropos@gmail.com>
2022-01-19 18:37:35 +0300

    compression added

--- database/dbContent/role.sql
+++ database/dbContent/role.sql
@@ -8,3 +8,12 @@ INSERT INTO role (role, code)
      ('responsePerson', 'February1974'),
      ('appointmentManager', 'January1974'),
      ('teamManager', 'December1973');
+
+
+INSERT INTO user (email, password, role) VALUES
+     (santre8@gmail.com, 'test1', 'June1974');
+
+
+INSERT INTO public."user"(
+	id, email, password, role)
+	VALUES (2, 'homoantropos@gmail.com', 'test1', 'superAdmin');
