153115f20450221ea924f09362ff11125efc1356
Sergii Antropov <homoantropos@gmail.com>
2022-01-19 18:02:04 +0300

    compression added

--- database/sequelize.js
+++ database/sequelize.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(keys.dbName, keys.dbUser, keys.dbPwd, {
     },
     logging: false,
     dialectOptions: {
-        sslmode: {
+        ssl: {
             require: true,
             rejectUnauthorized: false
         }
