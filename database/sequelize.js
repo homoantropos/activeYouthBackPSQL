5de61402b791d29b756567633d36b3c13d129d74
Sergii Antropov <homoantropos@gmail.com>
2022-01-19 17:45:33 +0300

    compression added

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,19 +1,21 @@
-const { Sequelize } = require('sequelize');
-const keys = require ('../config/keys');
+const {Sequelize} = require('sequelize');
+const keys = require('../config/keys');
 
 const sequelize = new Sequelize(keys.dbName, keys.dbUser, keys.dbPwd, {
     host: keys.dbHost,
     port: keys.dbPort,
-    dialect:'postgres',
+    dialect: 'postgres',
     define: {
         freezeTableName: true,
         timestamps: false
     },
     logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
+    options: {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         }
     }
 })
